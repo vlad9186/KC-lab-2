2380831627b2b5b1176ca437a910e12ace124785
vlad9186 <32648658+vlad9186@users.noreply.github.com>
2019-04-02 15:55:36 +0300

    Add files via upload

---
+++ AlgByta/AlgByta/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgByta
+{
+    public class BoothAlgorithm
+
+    {
+        public int boothMultiply(int multiplicand, int multiplier)
+        {
+            int[] m = toBinary(multiplicand); // Multiplicand to binary
+            int[] m2 = toBinary(-multiplicand); // 2's Complement of multiplicand
+            int[] q = toBinary(multiplier); // Multiplier to binary
+            int[] Addition = new int[17]; //Binary Array for positive Multiplicand used in addition
+            int[] Subtraction = new int[17]; //Binary Array for negative Multiplicand used in addition
+            int[] ProductReg = new int[17]; // Binary Array for product register with extra bit for initializing (17)
+
+            // Fill Addition and Subtraction Arrays from the Left
+            for (int i = 0; i < 16; i++)
+            {
+                Addition[i] = m[i];
+                Subtraction[i] = m2[i];
+            }
+
+            // Fill the Product Register from the right
+            for (int i = 8; i <= 16; i++)
+            {
+                ProductReg[i] = q[i - 8];
+            }
+
+            // Make sure to Initilize
+            ProductReg[16] = 0;
+
+            //Display the results
+
+            steps(Addition, "A");
+            steps(Subtraction, "S");
+            steps(ProductReg, "P");
+            Console.WriteLine(" ");
+
+            // Checks the product register last two bits to choose which action for current step
+            for (int i = 0; i < 8; i++)
+            {
+                if (ProductReg[15] == 0 && ProductReg[16] == 0)
+                {
+                    // 00 means do nothing
+                }
+                if (ProductReg[15] == 1 && ProductReg[16] == 1)
+                {
+                    // 11 means do nothing
+                }
+                if (ProductReg[15] == 1 && ProductReg[16] == 0)
+                {
+                    // 10 means to subtract (add negative multiplicand)
+                    steps(Subtraction, "S");
+                    add(ProductReg, Subtraction);
+
+                }
+                if (ProductReg[15] == 0 && ProductReg[16] == 1)
+                {
+                    // 01 means to add
+                    steps(Addition, "A");
+                    add(ProductReg, Addition);
+                }
+                // Shiftin over in each s
+                shiftRight(ProductReg);
+                steps(ProductReg, "P");
+                Console.WriteLine();
+            }
+
+            // Converts the Product register to a string
+            string original = string.Join("", ProductReg);
+
+            // Checks the length to remove the initializer index
+            if (original.Length >= 17)
+            {
+                string binaryResult = original.Remove(original.Length - 1);
+                Console.WriteLine("Binary Result: " + binaryResult);
+                return Convert.ToInt32(binaryResult, 2);
+            }
+            else
+            {
+                Console.WriteLine("Binary Result: " + original);
+                return Convert.ToInt32(original, 2);
+            }
+
+        }
+
+        // Shifts the Product register to the right
+        public void shiftRight(int[] X)
+        {
+            // Shifts the product register to the right
+            for (int i = 16; i >= 1; i--)
+            {
+                X[i] = X[i - 1];
+            }
+        }
+
+        // Adds the Subtraction or Addition Register
+        public void add(int[] X, int[] Y)
+        {
+            int carryBit = 0;
+            for (int i = 8; i >= 0; i--)
+            {
+                int temporary = X[i] + Y[i] + carryBit;
+                X[i] = temporary % 2;
+                carryBit = temporary / 2;
+            }
+
+        }
+
+        // Converts to binary
+        public int[] toBinary(int number)
+        {
+            int n = number;
+
+            // In C#, to String converts to base 2 and utilizes 2s complement
+            // Comes in as 32 bits
+            var bin = Convert.ToString(n, 2);
+
+            // For negative numbers
+            if (n < 0)
+            {
+                // Trims the extra 1's and adds 0 to index 16
+                if (bin.Length == 32)
+                {
+                    bin = bin.Remove(1, 24);
+                    bin = bin.PadRight(16, '0');
+                }
+                // converts to an array
+                return bin.Select(c => int.Parse(c.ToString())).ToArray();
+            }
+
+            if (bin.Length == 32)
+            {
+                bin = bin.Remove(0, 8);
+                // Shifts over to accomadate for it being on the right
+                bin = bin.PadRight(16, '0');
+                // converts to an array
+                return bin.Select(c => int.Parse(c.ToString())).ToArray();
+            }
+
+            if (bin.Length < 8)
+            {
+                // If its lesss make it 8 bits long
+                bin = bin.PadLeft(8, '0');
+                // Shifts over to accomadate for it being on the right
+                bin = bin.PadRight(16, '0');
+                //// Converts string into an int array
+                return bin.Select(c => int.Parse(c.ToString())).ToArray();
+            }
+            return bin.Select(c => int.Parse(c.ToString())).ToArray();
+        }
+
+        // Displays the steps
+        public void steps(int[] X, string name)
+        {
+            Console.Write(name + ": ");
+
+            for (int i = 0; i < X.Length; i++)
+            {
+
+                if (i == 8)
+                {
+                    Console.Write(" ");
+                }
+
+                if (i == 16)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write(X[i]);
+            }
+            Console.WriteLine();
+
+        }
+
+        // MAIN 
+        public static void Main(string[] args)
+        {
+            // Initialize variables to pass in
+            int M = 0; // Multiplicand
+            int Q = 0; // Multiplier
+            int R = 0; // Result
+
+            Console.WriteLine("Booth Algorithm");
+            BoothAlgorithm ba = new BoothAlgorithm();
+            Console.WriteLine("If input bin pres \"b\" if dex press any one other button ");
+            if (Console.ReadLine() != "b")
+            {
+                // Have user input two integers for Multiplicand & Multiplier
+                Console.WriteLine("\nPlease enter an Multiplier: ");
+                Q = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Convert.ToString(Q, 2));           
+                Console.WriteLine("Please enter an Multiplicand: ");
+                M = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Convert.ToString(M, 2));
+            }
+            else
+            {
+                Console.WriteLine("Please enter an Multiplier: ");
+                Q = Convert.ToInt32(Console.ReadLine(), 2);
+                Console.WriteLine("Dec " + Q);
+                Console.WriteLine("Please enter an Multiplicand: ");
+                M = Convert.ToInt32(Console.ReadLine(), 2);
+                Console.WriteLine("Dec " + M);
+            }
+            Console.WriteLine("Answer: " + M + "*" + Q);
+            // Perform Booths
+            R = ba.boothMultiply(M, Q);
+            int decResult = M * Q;
+            Console.WriteLine("Decimal Result: " + decResult + " Booth Algorithm Result " + R);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ IEEE/IEEE/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEEE
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input first number");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("input second number");
+            string num2 = Console.ReadLine();
+            floatingPoint(num1, num2);
+            Console.ReadKey();
+        }
+        public static void floatingPoint(string num1, string num2)
+        {
+            float first, second;
+            first = float.Parse(num1);
+            second = float.Parse(num2);
+
+            long firstBits = GetFloatBits(first),
+            secondBits = GetFloatBits(second);
+
+            int sign1 = (int)((firstBits >> 31) & 1),
+            sign2 = (int)((secondBits >> 31) & 1);
+
+            int expo1 = (int)((firstBits >> 23) & 255),
+            expo2 = (int)((secondBits >> 23) & 255);
+
+            long mantissa1 = firstBits & ((int)Math.Pow(2, 23) - 1),
+            mantissa2 = secondBits & ((int)Math.Pow(2, 23) - 1);
+            Console.WriteLine("\nencoding the specified values in ieee 754:");
+            Console.WriteLine("X: " + Convert.ToString(Convert.ToInt32(sign1), 2) + " " + Convert.ToString(Convert.ToInt32(expo1), 2) + " " + Convert.ToString(Convert.ToInt32(mantissa1), 2));
+            Console.WriteLine("Y: " + Convert.ToString(Convert.ToInt32(sign2), 2) + " " + Convert.ToString(Convert.ToInt32(expo2), 2) + " " + Convert.ToString(Convert.ToInt32(mantissa2), 2) + " \n\n Align binary points: ");
+            Console.WriteLine("is will need aditional bit: " + (mantissa1 + mantissa2 > Math.Pow(2, 23) && (first - Math.Abs(first) == 0) && (second - Math.Abs(second) == 0)) + "(pred Normalize result)");
+            int new_expo;
+            string new_mantisa1, new_mantisa2;
+            if (mantissa1 + mantissa2 > Math.Pow(2, 23) && (first - Math.Abs(first) == 0) && (second - Math.Abs(second) == 0))
+            {
+                new_expo = Math.Max(expo1, expo2) + 1;
+            }
+            else
+            {
+                new_expo = Math.Max(expo1, expo2);
+            }
+
+            new_mantisa1 = Convert.ToString(Convert.ToInt32(mantissa1 >> new_expo - expo1), 2);
+            new_mantisa2 = Convert.ToString(Convert.ToInt32(mantissa2 >> new_expo - expo2), 2);
+            Console.WriteLine("max expo from x and y: " + Math.Max(expo1, expo2) + " \nnew expo: " + new_expo + " bin: " + Convert.ToString(Convert.ToInt32(new_expo), 2));
+            new_mantisa1 = add_mant_to_23(new_mantisa1, new_expo - expo1 - 1);
+            new_mantisa2 = add_mant_to_23(new_mantisa2, new_expo - expo2 - 1);
+
+            Console.WriteLine("X: " + Convert.ToString(Convert.ToInt32(sign1), 2) + " " + Convert.ToString(Convert.ToInt32(new_expo), 2) + " " + new_mantisa1);
+            Console.WriteLine("Y: " + Convert.ToString(Convert.ToInt32(sign2), 2) + " " + Convert.ToString(Convert.ToInt32(new_expo), 2) + " " + new_mantisa2);
+            string sum_m1_m2 = add(new_mantisa1, new_mantisa2);
+            int sum_man;
+            sum_man = Convert.ToInt32(sum_m1_m2, 2);
+            Console.WriteLine("\nAdd significands:\nnew mantisa: " + Convert.ToString(sum_man, 2));
+
+            int resultMask = sum_man;
+            resultMask |= new_expo << 23;
+            resultMask |= sign1 << 31;
+
+            byte[] bytes = new byte[4];
+            bytes[0] = (byte)(resultMask & 255);
+            bytes[1] = (byte)((resultMask >> 8) & 255);
+            bytes[2] = (byte)((resultMask >> 16) & 255);
+            bytes[3] = (byte)((resultMask >> 24) & 255);
+
+            float result = BitConverter.ToSingle(bytes, 0);
+            Console.WriteLine("Result: " + FinishStringWithZeros(Convert.ToString(resultMask, 2), 32) + " ( " + result + " ) ");
+
+
+        }
+
+        static string add_mant_to_23(string man, int p)
+        {
+            while (man.Length < 23)
+            {
+
+                man = '0' + man;
+            }
+            string res = string.Empty;
+            for (int i = 0; i < 23; i++)
+            {
+                if (i == p)
+                {
+                    res += '1';
+                }
+                else
+                {
+                    res += man[i];
+                }
+            }
+
+            return res;
+        }
+
+        static int GetFloatBits(float num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            int res = 0;
+            res |= bytes[0];
+            res |= bytes[1] << 8;
+            res |= bytes[2] << 16;
+            res |= bytes[3] << 24;
+            return res;
+        }
+
+        static int[] toBinary(int number)
+        {
+            int n = number;
+            var bin = Convert.ToString(n, 2);
+            return bin.Select(c => int.Parse(c.ToString())).ToArray();
+        }
+
+        static string FinishStringWithZeros(string val, int bitcount)
+        {
+            int count = bitcount - val.Length;
+            string head = "";
+            for (int i = 0; i < count; ++i)
+                head += "0";
+            return head + val;
+        }
+        static string add(string X, string Y)
+        {
+            string res = string.Empty;
+            int carryBit = 0;
+            for (int i = 22; i > -1; i--)
+            {
+                int temporary = str_to_int(X[i]) + str_to_int(Y[i]) + carryBit;
+                res = Convert.ToString(temporary % 2) + res;
+                carryBit = temporary / 2;
+            }
+            return res;
+        }
+        static int str_to_int(char s)
+        {
+            if (s == '0')
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
